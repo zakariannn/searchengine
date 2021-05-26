0db33fc18476d162083fdc7a2a62706671e71a64
zakariannn <marina.zakaryan.5@gmail.com>
2021-05-26 10:25:56 +0400

    Final Touches

--- Crawler/LinkExtractor/LinkExtractor.hpp
+++ Crawler/LinkExtractor/LinkExtractor.hpp
@@ -11,7 +11,7 @@ class LinkExtractor
 public:
     LinkExtractor() = default;
 
-    // extract all links from cur Document
+    // extract all links from current Document
     std::vector<std::string> extract(HtmlDocument& doc);
 
     std::vector<std::pair<std::string, std::string> > checkByDomain(std::vector<std::string>& links, const std::string& domain);
